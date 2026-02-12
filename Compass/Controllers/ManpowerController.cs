3dfc62a1d2c7384c2da348de285a7f11e8a82958
subirhtis <subir.kumar@horizontelecom.in>
2026-02-12 13:10:48 +0530

    Manpower Controller created

---
+++ Compass/Controllers/ManpowerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Compass.Controllers
+{
+    public class ManpowerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
