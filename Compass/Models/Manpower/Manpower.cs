0b16d5c5e05304c20700f2e4f7f4be0b589f3c9a
subirhtis <subir.kumar@horizontelecom.in>
2026-02-19 19:08:22 +0530

    Manpower Model

--- Compass/Models/Manpower/Manpower.cs
+++ Compass/Models/Manpower/Manpower.cs
@@ -1,6 +1,36 @@
-﻿namespace Compass.Models.Manpower
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Compass.Models.Manpower
 {
-    public class Manpower
+    public class WorkOrder
     {
+        public int WorkOrderAgencyId { get; set; }
+        public int AgencyId { get; set; }
+        public string AgencyName { get; set; }
+        public int DeptId { get; set; }
+        public string DepartmentName { get; set; }
+        public string WorkOrderNo { get; set; }
+        public string BillingId { get; set; }
+        public string BillingAddress { get; set; }
+        public int NoDeployedRes { get; set; }
+        public string BillAddressEmail { get; set; }
+        public int CreatedBy { get; set; }
+        public char IsActive { get; set; }
+        public char IsResourceUploaded { get; set; }
+        public int NoOfUploadedResource { get; set; }
+
+
     }
+
+    public class WO_Filter
+    {
+        public int WorkOrderAgencyId { get; set; }
+        public int AgencyId { get; set; }
+        public int DeptId { get; set; }
+        public int CreatedBy { get; set; }
+        public int UserRole { get; set; }
+
+
+    }
+
 }
