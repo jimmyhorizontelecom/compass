acd259dc1577b7ab204e9b7ac68fd3285393e6bb
subirhtis <subir.kumar@horizontelecom.in>
2026-08-24 20:48:25 +0530

    k

--- Compass/wwwroot/PageAssets/Hardware/Js/HardwareMaster/SaleOrder.js
+++ Compass/wwwroot/PageAssets/Hardware/Js/HardwareMaster/SaleOrder.js
@@ -1064,17 +1064,16 @@ async function SubmitConsigneeRecord() {
         if (!row.find(".rowCheckbox").is(":checked")) {
             return;
         }
-        let deliveryQty = parseFloat(row.find(".deliveryQty").val()) || 0;
         let selectedData = {
             ItemDetailsId: parseInt(row.data("itemdetailsid")) || 0,
             SaleOrderId: parseInt(OrderId) || 0,
-             ProductId: parseInt( row.data("productid")) || 0,
-            DeliveryQty: deliveryQty,
+            ProductId: parseInt(row.data("productid")) || 0,
+            DeliveryQty: parseFloat(row.find(".deliveryQty").val()) || 0,
              ConsigneeName: ConsigneeName,
              ConsigneeContactNo:  ContactNo,
              consigneeAddress: ConsigneeAddress,
             DistrictId: DistrictId,
-            DeliveredQty: deliveryQty,
+            DeliveredQty: 0,
         };
         selectedItems.push(selectedData);
     });
